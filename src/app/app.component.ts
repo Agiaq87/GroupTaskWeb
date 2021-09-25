6510b3129eeab75fefa26a71c310709fb53ef43d
jjnapolijjnapoli <jjnapolijjnapoli@gmail.com>
2021-09-25 16:44:19 +0200

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {TaskService} from "./service/task.service";
 
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,9 +10,39 @@ import {TaskService} from "./service/task.service";
 })
 export class AppComponent {
   title = 'Group Task';
-  tasks: TaskService;
+ 
+  login:boolean=false;
+password:any
+email:any
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]
+  );
+  passwordControl = new FormControl('', [Validators.required])
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+  matcher = new ErrorStateMatcher();
 
-  constructor(tasks: TaskService) {
-    this.tasks = tasks
+
+  constructor() {
+    
+  
+  } 
+  
+  submitLogin(){
+    //inserire chiamata login
+    // 
+    console.log(this.passwordControl.value)
+    console.log(this.emailFormControl.value)
+  let log={
+      email:this.emailFormControl.value,
+      password:this.emailFormControl.value
+    }
+    this.login=true;
+    //chiama servizio con oggetto log inserire nel caso di true this.login =true; in caso di false prevedere
+    //un messaggio di errore 
   }
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../service/task.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass']
+}) 
+export class HomeComponent implements OnInit {
+  title = 'Group Task';
+  tasks: TaskService;
+  login:boolean=false;
+
+  constructor(tasks: TaskService) {
+    this.tasks = tasks
+  }
+
+  ngOnInit(): void {
+  }
+
+}
